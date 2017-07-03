29b5a81609b1a3ca6b45a06acc9115bb6aaab866
Juferz <juan.isfg@gmail.com>
2017-07-03 14:59:57 -0500

    vista para el reglamento del hotel

---
+++ resources/views/reglamento.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.master')
+@section('titulo') Sobre Nosotros @endsection
+
+@section('contenido')
+
+    <section class="section-breadcrumb">
+        <h2 class="title">Reglamento del Hotel</h2>
+    </section>
+    <section class="section-style-2">
+        <div class="container">
+            <p class="content">
+                <strong>1.</strong>  Todo daño o perdida causada por el huésped a los bienes, inmuebles y enseres propiedad del hotel deberá pagarlo de acuerdo con el valor establecido por la empresa.
+                <br><br>
+                <strong>2.</strong>   Los objetos de valor deben ser reportados previamente, para que en caso de pérdida esta sea cubierta por la póliza hotelera, de lo contrario la administración no se responsabiliza por estos. 
+                <br><br>
+                <strong>3.</strong>   Para registrarse en el hotel, toda persona adulta debe identificarse mediante cédula de ciudadanía, extranjería o pasaporte vigentes, igualmente los menores de edad con registro civil o tarjeta de identidad. 
+                <br><br>
+                <strong>4.</strong>   El hotel se reserva el derecho de pedir la evacuación de cualquier persona ajena que estuviere en las instalaciones sin autorización.
+                <br><br>
+                <strong>5.</strong>   El hotel no se responsabiliza por el consumo de alimentos y bebidas fuera de nuestras instalaciones. 
+                <br><br>
+                <strong>6.</strong>   El hotel no se responsabiliza por objetos olvidados durante la estadía en áreas sociales y en habitaciones. 
+                <br><br>
+                <strong>7.</strong>   No se permite música estridente o con alto volumen en los jardines, ni en los salones o habitaciones.
+                <br><br>
+                <strong>8.</strong>   Queda terminantemente prohibido fumar en cualquier área del hotel. Ley 1335 de 2009. 
+                <br><br>
+                <strong>6.</strong>   El hotel no se hace responsable por la pérdida parcial o total de bienes, valores o vehículos en parqueaderos recomendados. 
+                <br><br>
+                <strong>10.</strong>   Todas las reservaciones se realizaran con un anticipo del 50%, si se realizan cancelaciones o modificaciones a esta debe ser al menos 3 días antes del inicio de la reservación, de lo contrario el dinero no será reembolsado. 
+            </p>
+        </div>
+    </section>
+
+
+@endsection
