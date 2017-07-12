ee78da3fc70965b258ab62611822be02ec99460c
Juferz <juan.isfg@gmail.com>
2017-07-11 20:34:34 -0500

    Controlador para envio de Mail

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Mail;
+use Session;
+use Redirect;
 
 class MailController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+    	Mail::send('contact',$request->all(), function($msj){
+    		$msj->subject('correo de contacto');
+    		$msj->to('ingsistemasjf@gmail.com');
+    	});
+    	Session::flash('message','Mensaje enviado correctamente');
+    	return Redirect::to('/contact');
+    }
+
+
+
+
 }
