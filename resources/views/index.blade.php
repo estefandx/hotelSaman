e7cd748b9ef15b4df6a5268fab0a6325c3944b77
Juferz <juan.isfg@gmail.com>
2017-07-16 21:34:48 -0500

    se modifica la vista del index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,50 +8,20 @@
             <h2 class="hidden">Main Slider</h2>
             <div class="swiper-wrapper">
                 @foreach( $photoHotel as $photo)
-                    <div class="swiper-slide" style="background: url('fotos/{{$photo->nombre}}') center top/cover no-repeat">
-                        <div class="container">
-                            <div class="slide-content">
-
-
-
-                             </div>
+                <div class="swiper-slide" style="background: url('fotos/{{$photo->nombre}}') center top/cover no-repeat">
+                    <div class="container">
+                        <div class="slide-content"></div>
                     </div>
                 </div>
                 @endforeach
-                <!-- Slide 01 -->
-                <!--
-                <div class="swiper-slide" style="background: url('images/home/slider-1.jpg') center top/cover no-repeat">
-                    <div class="container">
-                        <div class="slide-content">
-                           <!-- <div class="slide-subtitle" data-animate="fadeInRight">BIENVENIDOS!</div>
-                            <h3 class="slide-title" data-animate="fadeInDown"><strong>EL Samán Viterbo</strong>Hotel</h3>
-                            <div class="slide-divider" data-animate="fadeInRight"></div>
-                            <div class="slide-subtitle-italic" data-animate="fadeInRight" data-delay="300">bringing to you the best experience from only <span class="price">$149</span> / night</div>
-                            <a href="#" class="button transparent" data-animate="fadeInRight" data-delay="300">Get this offer</a>-->
-                   <!--     </div>
-                    </div>
-                </div>
-                <!-- Slide 02 -->
-              <!-- <div class="swiper-slide" style="background: url('images/home/slider-2.png') center top/cover no-repeat">
-                    <div class="container">
-                        <div class="slide-content">
-                           <!-- <div class="slide-subtitle" data-animate="fadeInRight">BIENVENIDOS!</div>
-                            <h3 class="slide-title" data-animate="fadeInDown"><strong>EL Samán Viterbo</strong>Hotel</h3>
-                            <div class="slide-divider" data-animate="fadeInRight"></div>
-                            <div class="slide-subtitle-italic">bringing to you the best experience from only <span class="price">$149</span> / night</div>
-                            <a href="#" class="button transparent">Get this offer</a>-->
-                      <!--  </div>
-                    </div>
-                </div>
-                -->
             </div>
         </div>
         <div class="main-slider-control prev"><i class="icon-prev"></i></div>
         <div class="main-slider-control next"><i class="icon-next"></i></div>
-        <div class="container">
-            <div class="page-controls centered clearfix">
+            <div class="container">
+                <div class="page-controls centered clearfix">
+                </div>
             </div>
-        </div>
     </div>
 
     <!-- seccion de las 3 imagenes para los negocios -->
@@ -62,21 +32,21 @@
             <div class="row">   
                 <div class="col-sm-4">
                     <div class="content-box"> 
-                        <a href="{{url('/restaurante')}}"> <img src="images/home/restaurante.png" class="img-centered img-responsive" data-animate="fadeIn" alt="why-moon-2"></a>
+                        <a href="{{url('/restaurante')}}"> <img src="images/home/restaurante.png" class="img-centered img-responsive" data-animate="fadeIn" alt="restaurante-las-tapas"></a>
                         <h3 class="title">Restaurante Las Tapas</h3>
                         <p class="content">Contamos con servicio de comida ejecutiva durante el día. En la noche, platos a la carta y comidas rápidas, Servicio a domicilio.</p>
                     </div>
                 </div>
                 <div class="col-sm-4">
                     <div class="content-box ">
-                        <a href="{{url('/piscinas')}}"><img src="images/home/polideportivo.png" class="img-centered img-responsive" data-animate="fadeIn" alt="why-moon-1"></a>
+                        <a href="{{url('/piscinas')}}"><img src="images/home/polideportivo.png" class="img-centered img-responsive" data-animate="fadeIn" alt="piscinas-saman"></a>
                         <h3 class="title">Piscinas Polideportivo</h3>
                         <p class="content">Espectacular lugar para que pases un dia en familia y disfrutes de un ambiente agradable en piscina</p>
                     </div>
                 </div>
                 <div class="col-sm-4">
                     <div class="content-box">
-                        <a href="{{url('/eventos')}}"><img src="images/home/eventos.png" class="img-centered img-responsive" data-animate="fadeIn" alt="why-moon-3"></a>
+                        <a href="{{url('/eventos')}}"><img src="images/home/eventos.png" class="img-centered img-responsive" data-animate="fadeIn" alt="eventos-saman"></a>
                         <h3 class="title">Samán Eventos</h3>
                         <p class="content ">Servicio especial de eventos, nos encargamos y atendemos cualquier tipo de eventos. Hacemos de sus eventos los mejores.</p>
                     </div>
@@ -96,7 +66,7 @@
                 <div class="col-md-6">
                     <div class="col-sm-6">
                         <div class="content-box">
-                            <img src="images/home/hotel.jpg" class="img-centered img-responsive" data-animate="zoomIn" alt="facility-1" width="310" height="200">
+                            <img src="images/home/hotel.jpg" class="img-centered img-responsive" data-animate="zoomIn" alt="hotel-saman" width="310" height="200">
                             <div class="tri-up"></div>
                             <h3 class="title">Excelente Ubicación</h3>
                             <p class="content">Estamos ubicados en uno de los mejores sectores de Viterbo, central y tranquilo, un lugar ideal para descansar.</p>
@@ -107,14 +77,14 @@
                             <h3 class="title">Ambiente Acogedor</h3>
                             <p class="content">Nuestras instalaciones estan adecuadas para que usted y su familia se sientan como en casa en cada uno de los espacios que le brindamos.</p>
                             <div class="tri-down"></div>
-                            <img src="images/home/pasillo.jpg" width="310" height="200" class="img-centered img-responsive" data-animate="zoomIn" alt="facility-2">
+                            <img src="images/home/pasillo.jpg" width="310" height="200" class="img-centered img-responsive" data-animate="zoomIn" alt="ambiente-el-saman">
                         </div>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="col-sm-6">
                         <div class="content-box">
-                            <img src="images/home/panel.png" class="img-centered img-responsive" data-animate="zoomIn" alt="facility-2">
+                            <img src="images/home/panel.png" class="img-centered img-responsive" data-animate="zoomIn" alt="energia-solar-hotel-saman">
                             <div class="tri-up"></div>
                             <h3 class="title">Cuidamos el Medio Ambiente</h3>
                             <p class="content">Nuestro principal compromiso es CUIDAR el Medio Ambiente, es por eso que en nuestro hotel el servicio de agua caliente se brinda por medio de energia generada por PANELES SOLARES.</p>
@@ -125,7 +95,7 @@
                             <h3 class="title">Habitaciones Múltiples</h3>
                             <p class="content">Contamos con habitaciones múltiples desde parejas hasta grupos con espacios amplios a excelente costo y siempre con la mejor comodidad.</p>
                             <div class="tri-down"></div>
-                            <img src="images/home/hab1.jpg" class="img-centered img-responsive" data-animate="zoomIn" alt="facility-4">
+                            <img src="images/home/hab1.jpg" class="img-centered img-responsive" data-animate="zoomIn" alt="habitaciones-saman">
                         </div>
                     </div>
                 </div>
