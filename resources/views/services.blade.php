05b98b6aba0d8bf918292c59f4506bbcf1bdf7c2
Juferz <juan.isfg@gmail.com>
2017-07-16 19:38:05 -0500

    edita vista

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -22,26 +22,62 @@
                     </div>
                     <div class="col-sm-6 col-md-3">
                         <div class="content-box">
-                            <img src="images/servicios/hab1.jpg" class="img-centered img-responsive" data-animate="zoomIn" alt="facility-2">
+                            <img src="images/servicios/panel.png" class="img-centered img-responsive" data-animate="zoomIn" alt="facility-2">
                             <div class="tri-up"></div>
-                            <h3 class="title">Cómodas Habitaciones</h3>
-                            <p class="content">Nuestras habitaciones cuentan con todo para que este siempre comodo, ducha con agua caliente, tv, wifi y muy buena ventilación para que su descanso sea mejor.</p>
+                            <h3 class="title">Cuidamos el Medio Ambiente</h3>
+                            <p class="content">Nuestro principal compromiso es CUIDAR el Medio Ambiente, es por eso que en nuestro hotel el servicio de agua caliente se brinda por medio de energia generada por PANELES SOLARES.</p>
                         </div>
                     </div>
                     <div class="col-sm-6 col-md-3">
                         <div class="content-box">
-                            <img src="images/servicios/hab2.jpg" class="img-centered img-responsive" data-animate="zoomIn" alt="facility-3">
+                            <img src="images/servicios/hab2.jpg" class="img-centered img-responsive" data-animate="zoomIn" alt="facility-2">
+                            <div class="tri-up"></div>
+                            <h3 class="title">Cómodas Habitaciones</h3>
+                            <p class="content">Nuestras habitaciones estan distribuidas de la mejor manera y con excelentes servicios como ducha con agua caliente, TV, wifi y muy buena ventilación para que su descanso sea el mejor.</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                        <div class="content-box">
+                            <img src="images/servicios/hab1.jpg" class="img-centered img-responsive" data-animate="zoomIn" alt="facility-3">
                             <div class="tri-up"></div>
                             <h3 class="title">Habitaciones Múltiples</h3>
                             <p class="content">Contamos con habitaciones múltiples desde parejas hasta grupos con espacios amplios a excelente costo y siempre con la mejor comodidad.</p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6 col-md-3">
+                        <div class="content-box">
+                            <img src="images/servicios/recepcion.jpg" class="img-centered img-responsive" data-animate="zoomIn" alt="facility-1">
+                            <div class="tri-up"></div>
+                            <h3 class="title">Recepción</h3>
+                            <p class="content">Nuestro servicio de recepción es 24 horas para estar siempre atentos a cualquier solicitud que requiera con prontitud.</p>
                         </div>
                     </div>
                     <div class="col-sm-6 col-md-3">
                         <div class="content-box">
                             <img src="images/servicios/pasillo.jpg" class="img-centered img-responsive" data-animate="zoomIn" alt="facility-4">
                             <div class="tri-up"></div>
-                            <h3 class="title">Nuestro Servicio</h3>
-                            <p class="content">Nuestras instalaciones estan adecuadas para que usted y su familia se sientan como en casa en cada uno de los espacios que le brindamos.</p>
+                            <h3 class="title">Nuestro Instalaciones</h3>
+                            <p class="content">Nuestras instalaciones estan adecuadas para que usted y su familia se sientan como en casa en cada uno de los espacios que brindamos.</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                        <div class="content-box">
+                            <img src="images/servicios/parqueadero.jpg" class="img-centered img-responsive" data-animate="zoomIn" alt="facility-4">
+                            <div class="tri-up"></div>
+                            <h3 class="title">Parqueadero</h3>
+                            <p class="content">Puede confiar su vehiculo en nuestro parqueadero para que siempre este a la mano y a su vez con la tranquilidad de tenerlo cerca y en un lugar seguro.</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                        <div class="content-box">
+                            <img src="images/servicios/restaurant.jpg" class="img-centered img-responsive" data-animate="zoomIn" alt="facility-4">
+                            <div class="tri-up"></div>
+                            <h3 class="title">Restaurante</h3>
+                            <p class="content">Servicio de restaurante con platos a la carta y ademas con platos tipicos de nuestra región. </p>
                         </div>
                     </div>
                 </div>
